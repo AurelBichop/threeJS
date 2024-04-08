e90512befaf772771a91d39f4ccbbabf8a330053
AurelBichop <linux.aurelien@gmail.com>
2024-04-08 20:33:18 +0200

    test Three Js

---
+++ scripts/test3D.js
@@ -0,0 +1,9 @@
+// Option 1: Import the entire three.js core library.
+import * as THREE from '../node_modules/three/build/three.module.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
